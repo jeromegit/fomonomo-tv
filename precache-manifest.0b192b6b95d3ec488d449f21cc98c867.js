1de1986c7fd30102e994f1193f2778059f66926f
Jerome Provensal <jerome@provensal.com>
2019-06-09 19:37:41 -0700

    Updates

---
+++ precache-manifest.0b192b6b95d3ec488d449f21cc98c867.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b9f3b2085250aaef9d869402b53837b",
+    "url": "/react-navbar-search-restapi-table/index.html"
+  },
+  {
+    "revision": "da2950df07e1411352a1",
+    "url": "/react-navbar-search-restapi-table/static/css/main.f8399ae0.chunk.css"
+  },
+  {
+    "revision": "e779818e2a62fbcb0ed5",
+    "url": "/react-navbar-search-restapi-table/static/js/2.f263b6c4.chunk.js"
+  },
+  {
+    "revision": "da2950df07e1411352a1",
+    "url": "/react-navbar-search-restapi-table/static/js/main.16d563f6.chunk.js"
+  },
+  {
+    "revision": "fcb2a4c0dc45fd6cf89e",
+    "url": "/react-navbar-search-restapi-table/static/js/runtime~main.e860d5cb.js"
+  }
+]);
