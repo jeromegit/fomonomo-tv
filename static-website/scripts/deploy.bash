92e59dc789b57ce9fdb1208164ba7f70b67413e4
Jerome Provensal <jerome@provensal.com>
2019-06-16 18:41:39 -0700

    made it deployable with AWS and removed static-website dir that was created

--- static-website/scripts/deploy.bash
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-stage=$1
-region=$2
-
-echo "*** Deploying website with stage of $stage in $region ***"
-
-serverless client deploy --stage $stage --region $region
--- static-website/scripts/remove.bash
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-stage=$1
-region=$2
-
-echo "*** Removing website with stage of $stage in $region ***"
-
-serverless client remove --stage $stage --region $region
